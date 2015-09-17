c8d16f94cf71c9caa20c18c8265169103245c76b
Manoz Debnath <manozdebnath@users.noreply.github.com>
2015-09-18 00:37:46 +0600

    Update problem.php

--- problem.php
+++ problem.php
@@ -2,29 +2,30 @@
 /**
 * Solution of a Pangram problem in php
 */
+
 function isPangram($strings)
 {
     foreach ($strings as $string) {
         $haystack = range('a', 'z');
-        foreach (str_split($string) as $char) {
+        foreach (str_split(strtolower($string)) as $char) {
             if (in_array($char, $haystack)) {
               $key = array_search($char,$haystack);
                 unset($haystack[$key]);
             }
         }
-        if(count($haystack) === 0 ){
+       // var_dump($haystack);
+        if(count($haystack)==0){
             echo 1;
-        }else
-        echo 0;
+        }
+        else echo 0;
     }
 }
 
 $tests = array(
-    "the quick brown fox jumps over the lazy dog",
+    "the quick Brown fox jumps over the lazy dog",
     "the quick brown fox jumped over the lazy dog",
     "the quick brown fox jumps over the lazy dog",
-    "the quik brown fox jumped over the lazy dog",
-    "the quick fox jumps over the lazy dog",
+    "the quiCk brown fox jumps over the lazy dog",
 );
 
  isPangram($tests);
